5775b41612e07178ca2f92f429e37157248b70c3
Ren Tabor <tabor.warren@gmail.com>
2013-07-02 20:17:10 -0400

    Created User and Menu association. Created User Menu migration. Performed migration for User menu table.

--- app/models/menu.rb
+++ app/models/menu.rb
@@ -1,6 +1,7 @@
 class Menu < ActiveRecord::Base
-  attr_accessible :description, :food_categories, :image_url, :rest_name, :category_ids
+  attr_accessible :description, :food_categories, :image_url, :rest_name, :category_ids, :user_ids
   
   has_and_belongs_to_many :categories
+  has_and_belongs_to_many :users
   
 end
