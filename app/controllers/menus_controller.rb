b0a49c8ea0b14cb1d7d8ce619592deed86848eb2
Ren Tabor <tabor.warren@gmail.com>
2013-07-06 14:48:01 -0400

    Added verification for user signed in when like buttons are displayed. Buttons still don't work :/

--- app/controllers/menus_controller.rb
+++ app/controllers/menus_controller.rb
@@ -23,6 +23,7 @@ class MenusController < ApplicationController
     respond_to do |format|
       format.html # show.html.erb
       format.json { render json: @menu }
+      
     end
   end
 
--- db/schema.rb
+++ db/schema.rb
@@ -1,65 +1,3 @@
-<<<<<<< HEAD
-# encoding: UTF-8
-# This file is auto-generated from the current state of the database. Instead
-# of editing this file, please use the migrations feature of Active Record to
-# incrementally modify your database, and then regenerate this schema definition.
-#
-# Note that this schema.rb definition is the authoritative source for your
-# database schema. If you need to create the application database on another
-# system, you should be using db:schema:load, not running all the migrations
-# from scratch. The latter is a flawed and unsustainable approach (the more migrations
-# you'll amass, the slower it'll run and the greater likelihood for issues).
-#
-# It's strongly recommended to check this file into your version control system.
-
-ActiveRecord::Schema.define(:version => 20130615183518) do
-
-  create_table "categories", :force => true do |t|
-    t.string   "name"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
-    t.integer  "menu_id"
-  end
-
-  add_index "categories", ["name"], :name => "index_categories_on_name"
-
-  create_table "categories_menus", :force => true do |t|
-    t.integer "category_id"
-    t.integer "menu_id"
-  end
-
-  create_table "menus", :force => true do |t|
-    t.string   "rest_name"
-    t.text     "description"
-    t.string   "food_categories"
-    t.string   "image_url"
-    t.datetime "created_at",      :null => false
-    t.datetime "updated_at",      :null => false
-  end
-
-  create_table "users", :force => true do |t|
-    t.string   "email",                  :default => "", :null => false
-    t.string   "encrypted_password",     :default => "", :null => false
-    t.string   "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          :default => 0
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip"
-    t.string   "last_sign_in_ip"
-    t.datetime "created_at",                             :null => false
-    t.datetime "updated_at",                             :null => false
-    t.string   "first_name"
-    t.string   "last_name"
-    t.string   "profile_name"
-  end
-
-  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
-  add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
-
-end
-=======
 # encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
@@ -125,4 +63,3 @@ ActiveRecord::Schema.define(:version => 20130702235858) do
   end
 
 end
->>>>>>> 4d7c5bf3e8b1243400fa08a3c355a04089cf4342
